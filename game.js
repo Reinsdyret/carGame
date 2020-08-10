209c5307fe7cb38f0d1f334ec0b72e4c17bc3b8b
Lars Møen Haukland <Lars.haukland@gmail.com>
2020-08-10 09:41:33 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -4,38 +4,42 @@ canvas.width = window.innerWidth;
 canvas.height= window.innerHeight;
 var ctx = canvas.getContext("2d");
 var radius = canvas.height / 2;
-ctx.translate(radius, radius);
-setInterval(1000, draw());
+var x = radius;
+setInterval(draw(),1000);
 
 
 function draw() {
+    ctx.clearRect(0,0,10000,10000);
     //enemyCar();
     drawLine(ctx,radius);
-    playerCar(ctx, radius);
+    playerCar(ctx, x);
+    window.requestAnimationFrame(draw);
     
 }
 
 function drawLine(ctx,radius) {
-    for(var i=-1000; i <= 1200; i += 40) {
+    var time = new Date();
+    for(var i=-1000+time.getMilliseconds(); i <= 2000; i += 200) {
         ctx.beginPath();
-        ctx.rect(radius,i,10,20);
+        ctx.rect(radius,i,12,50);
         ctx.stroke();
     }
+    
     ctx.beginPath();
     ctx.lineWidth = 1;
     ctx.moveTo(radius-200,-1000);
-    ctx.lineTo(radius-200,1000);
+    ctx.lineTo(radius-200,2000);
     ctx.stroke();
     ctx.beginPath();
     ctx.lineWidth = 1;
     ctx.moveTo(radius+200,-1000);
-    ctx.lineTo(radius+200,1000);
+    ctx.lineTo(radius+200,2000);
     ctx.stroke();
     
 }
 
-function playerCar(ctx, radius) {
+function playerCar(ctx, x) {
     ctx.beginPath();
-    ctx.rect(radius+60,200,100,200);
+    ctx.rect(x+60,600,100,200);
     ctx.stroke();
 }
