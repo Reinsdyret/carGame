c7e5ed8165424d84b7108134a941c112ea7d1446
Lars Møen Haukland <Lars.haukland@gmail.com>
2020-08-16 13:24:16 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,23 +1,33 @@
-
 var canvas = document.getElementById("canvas");
+document.addEventListener('keydown',changeOffset);
 canvas.width = window.innerWidth;
 canvas.height= window.innerHeight;
 var ctx = canvas.getContext("2d");
 var radius = canvas.height / 2;
 var x = radius;
+var carXoffset = 60;
+var carYoffset = 0;
+var farge;
 setInterval(draw(),1000);
+
+var synne = prompt('Er du synne?');
+
+if(synne == 'ja') {
+    farge = '#FFC0CB';
+}else{farge = '#000000'}
 
 
 function draw() {
     ctx.clearRect(0,0,10000,10000);
     //enemyCar();
-    drawLine(ctx,radius);
-    playerCar(ctx, x);
+    drawLine();
+    playerCar();
     window.requestAnimationFrame(draw);
+    console.log(carYoffset);
     
 }
 
-function drawLine(ctx,radius) {
+function drawLine() {
     var time = new Date();
     for(var i=-1000+time.getMilliseconds(); i <= 2000; i += 200) {
         ctx.beginPath();
@@ -38,8 +48,35 @@ function drawLine(ctx,radius) {
     
 }
 
-function playerCar(ctx, x) {
+function playerCar() {
     ctx.beginPath();
-    ctx.rect(x+60,600,100,200);
-    ctx.stroke();
+    ctx.rect(x+carXoffset,600+carYoffset,100,200);
+    ctx.fillStyle = farge;
+    ctx.fill();
+}
+
+function changeOffset(e){
+    console.log(e.code);
+    switch(e.code){
+        case('ArrowLeft'):
+            if(carXoffset>=-195){
+                carXoffset -= 7;
+                break;
+            }else{break;}
+        case('ArrowRight'):
+            if(carXoffset<=95){
+                carXoffset += 7;
+                break;
+            }else{break;}
+        case('ArrowUp'):
+            if(carYoffset>=-550){
+                carYoffset -= 7;
+                break;
+            }else{break;}
+        case('ArrowDown'):
+            if(carYoffset<=550){
+                carYoffset+=7;
+                break;
+            }else{break;}
+    }
 }
